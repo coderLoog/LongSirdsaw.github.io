eabf61f1f029b71f90c3018171bab4da2b1dc84a
LongSirdsaw <2572228906@qq.com>
2021-10-17 21:46:21 +0800

    Upload

--- Js/ofo.js
+++ Js/ofo.js
@@ -1,4 +1,8 @@
-var eyeLeftPosition = {
+function browserRedirect() {
+           var sUserAgent = navigator.userAgent.toLowerCase();
+           if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(sUserAgent)) {
+               //跳转移动端页面
+               var eyeLeftPosition = {
   start: [70, 78],
   end: [100, 110]
 };
@@ -42,3 +46,26 @@ if (window.DeviceOrientationEvent) {
 } else {
   document.querySelector('body').innerHTML = '浏览器不支持DeviceOrientationEvent';
 }
+           } else {
+            var eyel = document.querySelectorAll('#eyeLeft');
+		var eyerde = document.querySelectorAll('#eyeRight');
+        window.addEventListener('mousemove',function(event){
+         
+            eyel.forEach(function(eyel){
+            let angle = Math.atan2(event.clientX-eyel.getBoundingClientRect()
+            .left-eyel.clientLeft/2,event.clientY-eyel.getBoundingClientRect().top
+            -eyel.clientTop/2);
+            let rot = angle * 180 / Math.PI * -1 - 90 ;
+            eyel.style.transform = `rotate(${rot}deg)`;
+               });
+			eyerde.forEach(function(eyerde){
+            let angle = Math.atan2(event.clientX-eyerde.getBoundingClientRect()
+            .left-eyerde.clientLeft/2,event.clientY-eyerde.getBoundingClientRect().top
+            -eyerde.clientTop/2);
+            let rot = angle * 180 / Math.PI * -1 - 90 ;
+            eyerde.style.transform = `rotate(${rot}deg)`;
+               })
+        })
+           }
+ }
+	browserRedirect(); 
